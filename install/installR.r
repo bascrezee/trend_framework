e6f7184fc3c5f6f1ebcf8b77fae1a696f4441542
bascrezee <bascrezee@gmail.com>
2019-01-30 14:02:15 +0100

    Revert "Dev"

--- install/installR.r
+++
@@ -1,6 +0,0 @@
-#!/net/exo/landclim/crezees/conda/envs/trends/bin/Rscript
-installdir <- "/net/exo/landclim/crezees/conda/envs/trends/lib/R/library/"
-install.packages("forecast",installdir,repos='http://cran.us.r-project.org')
-install.packages("bfast",installdir,repos='http://cran.us.r-project.org')
-install.packages("iki.dataclim",installdir,repos='http://cran.us.r-project.org')
-install.packages("trend",installdir,repos='http://cran.us.r-project.org')
